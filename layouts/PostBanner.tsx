d246e36d0004b286017348ed0dc962c537954319
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2024-11-01 13:23:44 +0530

    image is shown in 75% width

--- layouts/PostBanner.tsx
+++ layouts/PostBanner.tsx
@@ -28,15 +28,15 @@ export default function PostMinimal({ content, next, prev, children }: LayoutPro
       <article>
         <div>
           <div className="space-y-1 pb-10 text-center dark:border-gray-700">
-            <div className="w-full">
+            <div className="relative pt-10">
+              <PageTitle>{title}</PageTitle>
+            </div>
+            <div className="mx-auto w-3/4 items-center justify-center">
               <Bleed>
                 <div className="relative aspect-[2/1] w-full">
                   <Image src={displayImage} alt={title} fill className="object-cover" />
                 </div>
               </Bleed>
-            </div>
-            <div className="relative pt-10">
-              <PageTitle>{title}</PageTitle>
             </div>
           </div>
           <div className="prose max-w-none py-4 dark:prose-invert">{children}</div>

3825a4c214ca558f3c0b3a45b6d08028f7f1c74a
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2024-11-02 12:02:31 +0530

    redirect to portfolio for projects

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: 'https://hari31416.github.io/Portfolio', title: 'Projects' }, // redirect to Portfolio
   { href: '/about', title: 'About' },
 ]
 

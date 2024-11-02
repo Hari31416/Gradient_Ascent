3bd9a9c4db6c1996ec75f8ee001110f5d2420f8b
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2024-11-02 10:37:27 +0530

    add variables for giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -57,10 +57,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'Hari31416/Gradient_Ascent',
+      repositoryId: 'R_kgDOM8LNQg',
+      category: 'General',
+      categoryId: 'DIC_kwDOM8LNQs4Cj6Hs',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
@@ -76,6 +76,7 @@ const siteMetadata = {
       themeURL: '',
       // This corresponds to the `data-lang="en"` in giscus's configurations
       lang: 'en',
+      inputPosition: 'top', // supported options: bottom, top
     },
   },
   search: {
